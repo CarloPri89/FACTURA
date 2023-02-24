41ef7c3cb30725b2e151937d08246054dde2ae3e
Carlos Junior Prieto Hermoza <125917753+CarloPri89@users.noreply.github.com>
2023-02-24 06:03:08 -1200

    FACTURACION

---
+++ Factura/splash pantalla de inicio/splash pantalla de inicio/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Static contar As Integer
+        If contar < 100 Then
+            contar = contar + 10
+            ProgressBar1.Value = contar
+
+
+        Else
+            Timer1.Stop()
+            contar = 0
+            principal.Show()
+            Me.Hide()
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ Factura/splash pantalla de inicio/splash pantalla de inicio/My Project/Form2.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form2
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Static contar As Integer
+        If contar < 100 Then
+            contar = contar + 10
+            ProgressBar1.Value = contar
+
+
+        Else
+            Timer1.Stop()
+            contar = 0
+            principal.Show()
+            Me.Hide()
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+
+    End Sub
+End Class
